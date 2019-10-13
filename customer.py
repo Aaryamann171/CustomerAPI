b2bc53360fac4eecfd68343c16e99be23d8b6d74
Aaryamann <aaryamann171@gmail.com>
2019-10-13 15:52:05 +0530

    inital commit

---
+++ customer.py
@@ -0,0 +1,32 @@
+import json
+#test py - pytest 13:52
+
+
+class Customer:
+
+    def __init__(self):
+        self.customers = []
+
+    def add_customer(self, name, location):
+        new_customer = {}
+        new_customer["name"] = name
+        new_customer["location"] = location
+        self.customers.append(new_customer)
+        print("Customer: {0}".format(new_customer))
+        return json.dumps(new_customer)
+
+    def del_customer(self, name):
+        found = False
+        for idx, book in enumerate(self.customers):
+            if book["name"] == name:
+                index = idx
+                found = True
+                del self.customers[idx]
+        print("customers: {0}".format(json.dumps(self.customers)))
+        return found
+
+    def get_all_books(self):
+        return self.customers
+
+    def json_list(self):
+        return json.dumps(self.customers)
