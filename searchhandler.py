053043f645df99a5f11abf5813be81cbbd19312e
Aaryamann Sharma <aaryamann171@gmail.com>
2019-10-19 18:20:12 +0530

    fixed search handler

--- searchhandler.py
+++ searchhandler.py
@@ -1,18 +1,15 @@
- 
 import tornado.web
 import customer
 import json
-
-
-class SearchHandler(tornado.web.RequestHandler):
-    def initialize(self, customers):
+class SearchHandler(tornado.web.RedirectHandler):
+    def initialize(self, customers):    
         self.customers = customers
-
+        
     def get(self):
-        name = self.get_argument("name")
+        name = self.get_argument('name')
         result = self.customers.search_customer(name)
         if result:
-            self.write("{} was found!".format(name))
+            self.write("customer title: {0} succsessfully".format(name))
             self.set_status(200)
         else:
             self.write("Customer '{0}' not found".format(name))
