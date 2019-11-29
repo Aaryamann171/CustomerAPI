8a4325eaff95238451649ec870d4b71f5b569ee9
Aaryamann Sharma <aaryamann171@gmail.com>
2019-11-29 17:23:04 +0530

    changes to search

--- searchhandler.py
+++ searchhandler.py
@@ -8,10 +8,10 @@ class SearchHandler(tornado.web.RedirectHandler):
         
     def get(self):
         name = self.get_argument('name')
-        location = self.get_argument('location')
+        # location = self.get_argument('location')
         result = self.customers.search_customer(name)
         if result:
-            self.write("customer title: {0} was found, their location is {1}".format(name,location))
+            self.write("customer title: {0} was found!".format(name))
             self.set_status(200)
         else:
             self.write("Customer: '{0}' was not found.".format(name))
