87c7aae2ba47a223a849d2037f1b1915b83b8d54
Aaryamann Sharma <aaryamann171@gmail.com>
2019-11-30 14:25:17 +0530

    some new changes to shell files

--- Source/docker-image-build.sh
+++ Source/docker-image-build.sh
@@ -1,4 +1,5 @@
-PATH="$PATH";/home/aryan/google-cloud-sdk/bin
-IMAGE_NAME=gcr.io/microservices/gpymicro-image:%BUILD_NUMBER%
+PATH="$PATH"
+#/home/aryan/google-cloud-sdk/bin
+IMAGE_NAME=gcr.io/microservices-1-258611/microservices-app-image:$BUILD_NUMBER
 cd /home/aryan/Desktop/CustomerAPI/Source
-gcloud builds submit --tag %IMAGE_NAME% .
+gcloud builds submit --tag $IMAGE_NAME .
--- Source/kubernetes-deploy.sh
+++ Source/kubernetes-deploy.sh
@@ -1,4 +1,5 @@
-PATH="$PATH";/home/aryan/google-cloud-sdk/bin
-IMAGE_NAME=gcr.io/microservices/gpymicro-image:%BUILD_NUMBER%
+PATH="$PATH"
+#/home/aryan/google-cloud-sdk/bin
+IMAGE_NAME=gcr.io/microservices-1-258611/microservices-app-image:$BUILD_NUMBER
 cd /home/aryan/Desktop/CustomerAPI/Source
-kubectl set image deployment/py-gmicro py-gmicro=%IMAGE_NAME%
+kubectl set image deployment/microservices-app microservices-app=$IMAGE_NAME
