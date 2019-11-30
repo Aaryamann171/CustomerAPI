8f4c316ea4aa246d0612bd388e781716055aac5d
Aaryamann Sharma <aaryamann171@gmail.com>
2019-11-30 11:23:06 +0530

    c

--- Source/docker-image-build.sh
+++ Source/docker-image-build.sh
@@ -1,4 +1,4 @@
-SET PATH="$PATH";/home/aryan/google-cloud-sdk/bin
-SET IMAGE_NAME=gcr.io/microservices/gpymicro-image:%BUILD_NUMBER%
+PATH="$PATH";/home/aryan/google-cloud-sdk/bin
+IMAGE_NAME=gcr.io/microservices/gpymicro-image:%BUILD_NUMBER%
 cd /home/aryan/Desktop/CustomerAPI/Source
 gcloud builds submit --tag %IMAGE_NAME% .
