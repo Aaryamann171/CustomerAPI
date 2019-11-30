a0752e78b356b3166fa4551eb20ff1f49f2f9ef2
Aaryamann Sharma <aaryamann171@gmail.com>
2019-11-30 09:17:09 +0530

    updated shell scripts for the update dir structure

--- Source/kubernetes-deploy.sh
+++ Source/kubernetes-deploy.sh
@@ -1,4 +1,4 @@
 SET PATH=%PATH%;/home/aryan/google-cloud-sdk/bin
 SET IMAGE_NAME=gcr.io/handle-books/gpymicro-image:%BUILD_NUMBER%
-cd /home/aryan/Desktop/CustomerAPI
+cd /home/aryan/Desktop/CustomerAPI/Source
 kubectl set image deployment/py-gmicro py-gmicro=%IMAGE_NAME%
--- Source/test-xml.sh
+++ Source/test-xml.sh
@@ -1,11 +1,11 @@
 SET PATH=%PATH%;/usr/lib/python3.7;/usr/lib/python3.7/venv/scripts
-del /home/aryan/Desktop/python_tests_xml/*.*
-del /home/aryan/Desktop/CustomerAPI/coverage.xml
-cd /home/aryan/Desktop/CustomerAPI
+del /home/aryan/Desktop/CustomerAPI/python_tests_xml/*.*
+del /home/aryan/Desktop/CustomerAPI/Source/coverage.xml
+cd /home/aryan/Desktop/CustomerAPI/Source
 REM /usr/lib/python3.7/python runtests.py
 REM /usr/lib/python3.7/python -m coverage run -m pytest && coverage xml -o coverage.xml
 REM /usr/lib/python3.7/python -m coverage html -d coverage
 python runtests.py
 python -m coverage run -m pytest && coverage xml -o coverage.xml
-copy /home/aryan/Desktop/CustomerAPI/ python_tests_xml\*.* /home/aryan/Desktop/python_tests_xml
-copy /home/aryan/Desktop/CustomerAPI/coverage.xml /home/aryan/Desktop/
+copy /home/aryan/Desktop/CustomerAPI/Source python_tests_xml\*.* /home/aryan/Desktop/CustomerAPI/python_tests_xml
+copy /home/aryan/Desktop/CustomerAPI/Source/coverage.xml /home/aryan/Desktop/CustomerAPI
